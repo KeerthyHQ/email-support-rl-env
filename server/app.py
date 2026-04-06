06f3b68bc946a6b99ea147a5948721686ef5e797
Keerthy <keerthy.builds@gmail.com>
2026-04-06 03:41:39 +0000

    server manual

--- server/app.py
+++ server/app.py
@@ -1,6 +1,25 @@
 #api endpoints for the server
+#manual method 
 
+from models import EmailAction,EmailObservation
 from server.environment import EmailEnvironment
+from fastapi import FastAPI
+
+app = FastAPI()
+env = EmailEnvironment()
+
+@app.get("/reset",response_model=EmailObservation)
+def reset():
+    return env.reset()
+
+@app.get("/step",response_model=EmailObservation)
+def step(action:EmailAction):
+    return env.step(action)
+
+
+
+#automatic method
+'''from server.environment import EmailEnvironment
 from openenv.core.env_server import create_fastapi_app 
 from models import EmailAction,EmailObservation
 
@@ -10,4 +29,4 @@ app=create_fastapi_app(
     EmailEnvironment,
     action_cls=EmailAction,
     observation_cls=EmailObservation
-)
+)'''
